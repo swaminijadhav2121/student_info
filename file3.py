e48146539240c8fad0dcbab2f51e875b2fdbfcbf
swaminijadhav2121 <swaminijadhav1121@gmail.com>
2026-07-21 20:32:01 +0530

    create one variable

---
+++ file3.py
@@ -0,0 +1,18 @@
+#using data type
+
+name = "shreya"
+age = 17
+percentage = 85.45
+is_student = False
+colors = ["red","brown","white","black"]
+number = (10,30,20,40)
+fruits = {"apple","mango","banana"}
+student= {"name":"shreya","age":"17"}
+print (name)
+print (age)
+print (percentage)
+print (is_student)
+print (colors)
+print (number)
+print (fruits)
+print (student)
